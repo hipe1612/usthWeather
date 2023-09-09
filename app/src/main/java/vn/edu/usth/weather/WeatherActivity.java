116c8dc3b29a273534d22db03c73d8be8f8142b1
hipe1612 <HiepNQ.BI12-156@st.usth.edu.vn>
2023-09-09 16:02:29 +0700

    create tab layout /toolbar for app

--- app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -1,25 +1,48 @@
 package vn.edu.usth.weather;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class WeatherActivity extends AppCompatActivity {
     private static  final String TAG = "MainActivity";
     @Override
-    public void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
-        ForecastFragment emptyFragment = new ForecastFragment();
+
+        /*ForecastFragment emptyFragment = new ForecastFragment();
 
         getSupportFragmentManager().beginTransaction().add(R.id.container,emptyFragment)
-                .commit();
+                .commit();*/
+
+        PagerAdapter adapter = new HomeFragmentPagerAdapter(getSupportFragmentManager(),this);
+        ViewPager pager = (ViewPager) findViewById(R.id.pager);
+        pager.setOffscreenPageLimit(3);
+        pager.setAdapter(adapter);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab);
+        tabLayout.setupWithViewPager(pager);
+
         Log.i(TAG,"onCreate() method called");
+
     }
 
     @Override
-    public  void onStart(){
+    protected void onStart(){
         super.onStart();
         Log.i(TAG,"onStart() method called");
     }
@@ -48,4 +71,39 @@ public class WeatherActivity extends AppCompatActivity {
         Log.i(TAG, "onDestroy() method called");
     }
 
+    public class HomeFragmentPagerAdapter extends FragmentPagerAdapter {
+        private final int PAGE_COUNT = 3;
+        private Context context;
+        private final String[] titles ;
+
+        public HomeFragmentPagerAdapter(FragmentManager fm, Context context) {
+            super(fm);
+            this.context = context;
+            titles = new String[]{context.getString(R.string.hanoi),context.getString(R.string.paris),context.getString(R.string.toulouse)};
+        }
+
+        @Override
+        public int getCount() {
+            return PAGE_COUNT;
+        }
+
+        @Override
+        public Fragment getItem(int page) {
+            switch (page) {
+                case 0:
+                    return WeatherAndForecastFragment.newInstance(context.getString(R.string.hanoi));
+                case 1:
+                    return WeatherAndForecastFragment.newInstance(context.getString(R.string.paris));
+                case 2:
+                    return WeatherAndForecastFragment.newInstance(context.getString(R.string.toulouse));
+            }
+            return new EmptyFragment();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int page) {
+
+            return titles[page];
+        }
+    }
 }
